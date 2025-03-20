8a4b46c9ca9f3367435b4e70f03d8648402d0e0b
yan anderson <anderson.iad@physcteh.edu>
2025-03-20 22:11:08 +0300

    adding files

--- cat_app/lib/screens/detail_screen.dart
+++ cat_app/lib/screens/detail_screen.dart
@@ -1,1 +1,46 @@
+import 'package:flutter/material.dart';
 
+class DetailScreen extends StatelessWidget {
+  final String imageUrl;
+  final String breedName;
+  final Map<String, dynamic>? breedInfo;
+
+  DetailScreen({required this.imageUrl, required this.breedName, this.breedInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(breedName),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(imageUrl),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Breed: $breedName', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 10),
+                  if (breedInfo != null) ...[
+                    Text('Description: ${breedInfo!['description']}'),
+                    SizedBox(height: 10),
+                    Text('Temperament: ${breedInfo!['temperament']}'),
+                    SizedBox(height: 10),
+                    Text('Origin: ${breedInfo!['origin']}'),
+                    SizedBox(height: 10),
+                    Text('Life Span: ${breedInfo!['life_span']}'),
+                    SizedBox(height: 10),
+                    Text('Weight: ${breedInfo!['weight']['metric']} kg'),
+                  ],
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- cat_app/lib/screens/home_scree.dart
+++ cat_app/lib/screens/home_scree.dart
@@ -1,1 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'detail_screen.dart';
+import 'widgets/like_button.dart';
 
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _imageUrl = '';
+  String _breedName = '';
+  int _likeCount = 0;
+  Map<String, dynamic>? _breedInfo;
+
+  Future<void> _fetchCat() async {
+    final response = await http.get(Uri.parse('https://api.thecatapi.com/v1/images/search?has_breeds=true'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body)[0];
+      setState(() {
+        _imageUrl = data['url'];
+        _breedName = data['breeds'][0]['name'];
+        _breedInfo = data['breeds'][0];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCat();
+  }
+
+  void _onLike() {
+    setState(() {
+      _likeCount++;
+      _fetchCat();
+    });
+  }
+
+  void _onDislike() {
+    setState(() {
+      _fetchCat();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cat App'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onHorizontalDragEnd: (details) {
+                if (details.primaryVelocity! > 0) {
+                  _onLike();
+                } else if (details.primaryVelocity! < 0) {
+                  _onDislike();
+                }
+              },
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      imageUrl: _imageUrl,
+                      breedName: _breedName,
+                      breedInfo: _breedInfo,
+                    ),
+                  ),
+                );
+              },
+              child: Image.network(_imageUrl),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              LikeButton(icon: Icons.thumb_down, onPressed: _onDislike),
+              Text('Likes: $_likeCount'),
+              LikeButton(icon: Icons.thumb_up, onPressed: _onLike),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
